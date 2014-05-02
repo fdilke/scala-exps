8c48284c3509802db9b900ebdee5600be3e0fb35
Casper devboxbackus.springer-sbm.com <devboxbackus.springer-sbm.com@springer.com>
2014-05-02 14:21:56 +0100

    more sequence comprehension tests

--- src/test/scala/com/fdilke/scala/SequenceComprehensionTests.scala
+++ src/test/scala/com/fdilke/scala/SequenceComprehensionTests.scala
@@ -48,5 +48,45 @@ class SequenceComprehensionTests extends FunSpec {
 // But can't go:
 //      (for (x : String <- new Foo) yield x) should be(7)
     }
+
+    it("can combine inputs from standard classes") {
+      val letters = List("a","b")
+      val numbers = List(1,2,3)
+
+      val combined = for(l <- letters ; n <- numbers) yield s"$l$n"
+      combined should be (List(
+        "a1", "a2", "a3", "b1", "b2", "b3"
+      ))
+    }
+
+    it("can combine inputs with flatMap") {
+      class Foo {
+        def map(f: Int => Int) = this
+        def flatMap(f: Int => Bar) = this
+      }
+      class Bar {
+        def map(f: Int => Int) = this
+      }
+      val foo = new Foo
+      val bar = new Bar
+
+      val combined = for(x <- foo ; y <- bar) yield x+y
+      combined should be (foo)
+    }
+
+    it("can combine inputs with flatMap and give meaningful results") {
+      case class Foo(value: Int) {
+        def map(f: Int => Int) = new Foo(f(value))
+        def flatMap(g: Int => Bar) = g(value)
+      }
+      case class Bar(value: Int) {
+        def map(f: Int => Int) = new Bar(f(value))
+      }
+      val foo = Foo(2)
+      val bar = Bar(3)
+
+      val combined = for(x <- foo ; y <- bar) yield x+y
+      combined should be (Bar(5))
+    }
   }
 }
