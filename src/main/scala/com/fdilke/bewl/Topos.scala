9d478956ee99d9aacaeaccf11f9154e366624e69
Felix Dilke <fdilkeuk@yahoo.co.uk>
2014-05-07 21:34:42 +0100

    added general product diagrams

--- src/main/scala/com/fdilke/bewl/Topos.scala
+++ src/main/scala/com/fdilke/bewl/Topos.scala
@@ -12,11 +12,24 @@ trait ToposArrow[DOT <: ToposDot[DOT, ARROW], ARROW <: ToposArrow[DOT, ARROW]] {
   def apply(arrow : ARROW) : ARROW
 }
 
-trait BiproductDiagram[DOT <: ToposDot[DOT, ARROW], ARROW <: ToposArrow[DOT, ARROW]] {
+trait ProductDiagram[DOT <: ToposDot[DOT, ARROW], ARROW <: ToposArrow[DOT, ARROW]] {
   val product: DOT
+  val projections: Seq[ARROW]
+  def multiply(arrows: ARROW*): ARROW
+}
+
+trait BiproductDiagram[DOT <: ToposDot[DOT, ARROW], ARROW <: ToposArrow[DOT, ARROW]]
+  extends ProductDiagram[DOT, ARROW] {
   val leftProjection: ARROW
   val rightProjection: ARROW
   def multiply(leftArrow: ARROW, rightArrow: ARROW): ARROW
+
+  override val product: DOT
+  override val projections: Seq[ARROW] = Seq(leftProjection, rightProjection)
+  override def multiply(arrows: ARROW*) = if (arrows.size == 2)
+    multiply(arrows(0), arrows(1))
+  else
+    throw new IllegalArgumentException("biproduct multiplication requires two arrows")
 }
 
 trait Topos[DOT <: ToposDot[DOT, ARROW], ARROW <: ToposArrow[DOT, ARROW]] {
