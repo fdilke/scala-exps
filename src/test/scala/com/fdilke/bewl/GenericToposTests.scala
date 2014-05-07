4fd4fb4dd9caab2dd6193b787aa3fd83f6ed0c39
Felix Dilke <fdilkeuk@yahoo.co.uk>
2014-05-07 21:24:39 +0100

    aspiration to chain products

--- src/test/scala/com/fdilke/bewl/GenericToposTests.scala
+++ src/test/scala/com/fdilke/bewl/GenericToposTests.scala
@@ -22,11 +22,11 @@ ARROW <: ToposArrow[DOT, ARROW]
   import fixtures._
 
   describe(s"The fixtures ${fixtures.getClass.getSimpleName}") {
-    it("should have distinct objects") {
+    it("has distinct objects") {
       Set(foo, bar, baz) should have size 3
     }
 
-    it("should have arrows whose sources and targets match their names") {
+    it("has arrows whose sources and targets match their names") {
       foo2bar.source shouldBe foo
       foo2bar.target shouldBe bar
 
@@ -48,12 +48,12 @@ abstract class GenericToposTests[
   import topos._
 
   describe(s"The topos ${topos.getClass.getSimpleName}") {
-    it("should have identity arrows which can be composed") {
+    it("has identity arrows which can be composed") {
       foo2bar(foo.identity) shouldBe foo2bar
       bar.identity(foo2bar) shouldBe foo2bar
     }
 
-    it("should be able to construct biproduct diagrams") {
+    it("can construct biproduct diagrams") {
       val barXbaz = bar x baz
 
       val productArrow = barXbaz.multiply(foo2bar, foo2baz)
@@ -72,8 +72,15 @@ abstract class GenericToposTests[
       bar.toConstant(foo2bar) shouldBe fooToI
     }
 
-//    it("has grey hair") {
-//      0 shouldBe 1
+//    it("can chain products") {
+//      val barXfooXbar = bar x foo x baz
+//      val productArrow = barXfooXbaz.multiply(foo2bar, foo.identity, foo2baz)
+//      productArrow.source shouldBe foo
+//      productArrow.target shouldBe barXfooXbaz.product
+//
+//      barXfooXbaz.projections(0)(productArrow) shouldBe foo2bar
+//      barXfooXbaz.projections(1)(productArrow) shouldBe foo.identity
+//      barXfooXbaz.projections(2)(productArrow) shouldBe foo2baz
 //    }
   }
 }
