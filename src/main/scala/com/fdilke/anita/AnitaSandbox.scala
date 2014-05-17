2a309bf4c468da4845fef48f16bfcd7d62153aaf
Felix Dilke <fdilkeuk@yahoo.co.uk>
2014-05-17 16:45:15 +0100

    Felix/Anita: make the multiplication table configurable

--- src/main/scala/com/fdilke/anita/AnitaSandbox.scala
+++ src/main/scala/com/fdilke/anita/AnitaSandbox.scala
@@ -1,21 +1,25 @@
 package com.fdilke.anita
 
 object AnitaSandbox extends App {
+  val rows = 9
+  val columns = 6
+
+
   print("  |")
-  for (i <- 1 to 5) {
+  for (i <- 1 to columns) {
     print(" " + i + " ")
   }
   println("")
   print("--+-")
 
-  for (i <- 1 to 5) {
+  for (i <- 1 to columns) {
     print("---")
   }
   println("")
 
-  for (i <- 1 to 5) {
+  for (i <- 1 to rows) {
     print(" " + i + "|")
-    for (j <- 1 to 5) {
+    for (j <- 1 to columns) {
       print("" + "%2d".format(i * j) + " ")
     }
     println("")
