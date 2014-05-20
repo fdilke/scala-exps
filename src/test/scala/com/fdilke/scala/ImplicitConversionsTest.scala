b247648334b0d40664db6cda267d7086c6c33d90
Felix Dilke <fdilkeuk@yahoo.co.uk>
2014-05-20 23:56:50 +0100

    tests for implicit conversions

---
+++ src/test/scala/com/fdilke/scala/ImplicitConversionsTest.scala
@@ -0,0 +1,27 @@
+package com.fdilke.scala
+
+import org.scalatest.FunSpec
+import org.scalatest.matchers.ShouldMatchers
+import ShouldMatchers._
+
+class ImplicitConversionsTest extends FunSpec {
+  describe("Implicit conversions") {
+    it("work by importing an object converting values to a wrapper") {
+
+      object SpecialMultImplicits {
+        implicit def Int2SpecialMult(value : Int) =
+          new SpecialMult(value)
+      }
+
+      class SpecialMult(value: Int) {
+        def **(exponent: Int) = Seq.fill(exponent)(value)./:(1)(_ * _)
+      }
+
+      new SpecialMult(2).**(3) shouldBe 8
+
+      import SpecialMultImplicits._
+
+      2 ** 3 shouldBe 8
+    }
+  }
+}
