6cee098d50f28a143429669fd1e2b765e44c6b53
Felix Dilke <fdilkeuk@yahoo.co.uk>
2014-05-21 20:39:29 +0100

    more tests for fancy multiplication

--- src/test/scala/com/fdilke/util/FancyMultiplicationTest.scala
+++ src/test/scala/com/fdilke/util/FancyMultiplicationTest.scala
@@ -14,12 +14,25 @@ class FancyMultiplicationTest extends FunSpec {
       3 ** 5 shouldBe 15
     }
 
+    it("has 0 as a zero") {
+      for(i <- 0 to 10) {
+          (0 ** i) shouldBe 0
+      }
+    }
+
     it("has 1 as an identity") {
       for(i <- 0 to 10) {
           (1 ** i) shouldBe i
       }
     }
-    
+
+    it("is increasing for multipliers > 1") {
+      for(i <- 2 to 10;
+          j <- 1 to 10) {
+        (i ** j) should be > j
+      }
+    }
+
     it("is commutative") {
       for(i <- 0 to 10;
           j <- 0 to 10) {
@@ -32,6 +45,14 @@ class FancyMultiplicationTest extends FunSpec {
           j <- 0 to 10;
           k <- 0 to 10) {
           ((i ** j) ** k) shouldBe (i ** (j ** k))
+      }
+    }
+
+    it("distributes over ^") {
+      for(i <- 0 to 10;
+          j <- 0 to 10;
+          k <- 0 to 10) {
+          (i ** (j ^ k)) shouldBe ((i ** j) ^ (i ** k))
       }
     }
   }
