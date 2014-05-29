c5a0cdba99d5d8cc25536c50f64845788930afcd
Felix Dilke <fdilkeuk@yahoo.co.uk>
2014-05-29 20:00:27 +0100

    simplified the type verbiage by enclosing everything in a topos class which genericizes it all

--- src/test/scala/com/fdilke/scala/MoreTypeExperiments.scala
+++ src/test/scala/com/fdilke/scala/MoreTypeExperiments.scala
@@ -4,65 +4,54 @@ package com.fdilke.scala
 
 object MoreTypeExperiments {
 
-  trait Terminal
+  trait Topos[
+    DOT[P] <: Dot[P],
+    ARROW[P, Q] <: Arrow[P, Q],
+    BIPRODUCT[P, Q] <: Biproduct[P, Q],
+    EXPONENTIAL[P, Q] <: Exponential[P, Q]
+  ] {
 
-  trait Dot[X,
-    DOT[P] <: Dot[P, DOT, ARROW, BIPRODUCT, EXPONENTIAL],
-    ARROW[P, Q] <: Arrow[P, Q, DOT, ARROW, BIPRODUCT, EXPONENTIAL],
-    BIPRODUCT[P, Q] <: Biproduct[P, Q, DOT, ARROW, BIPRODUCT, EXPONENTIAL],
-    EXPONENTIAL[P, Q] <: Exponential[P, Q, DOT, ARROW, BIPRODUCT, EXPONENTIAL]
+    trait Terminal
+
+    trait Dot[X
     ] {
-    def identity: ARROW[X, X]
+      def identity: ARROW[X, X]
 
-    def toConstant: ARROW[X, Terminal]
+      def toConstant: ARROW[X, Terminal]
 
-    def x[Y](that: DOT[Y]): BIPRODUCT[X, Y]
+      def x[Y](that: DOT[Y]): BIPRODUCT[X, Y]
 
-    def ^[Y](that: DOT[Y]): EXPONENTIAL[Y, X]
+      def ^[Y](that: DOT[Y]): EXPONENTIAL[Y, X]
+    }
+
+    trait Arrow[X, Y
+    ] {
+      val source: DOT[X]
+      val target: DOT[Y]
+
+      def apply[W](arrow: ARROW[W, X]): ARROW[W, Y]
+    }
+
+    trait Biproduct[X, Y
+    ] {
+      val leftProjection: ARROW[(X, Y), X]
+      val rightProjection: ARROW[(X, Y), Y]
+
+      def multiply[W](leftArrow: ARROW[W, X],
+                      rightArrow: ARROW[W, Y]): ARROW[W, (X, Y)]
+    }
+
+    trait Exponential[S, T
+    ] {
+      val evaluation: BiArrow[S => T, S, T]
+
+      def transpose[W](multiArrow: BiArrow[W, S, T]): ARROW[W, S => T]
+    }
+
+    case class BiArrow[L, R, T
+    ](
+       product: BIPRODUCT[L, R],
+       arrow: ARROW[(L, R), T])
+
   }
-
-  trait Arrow[X, Y,
-    DOT[P] <: Dot[P, DOT, ARROW, BIPRODUCT, EXPONENTIAL],
-    ARROW[P, Q] <: Arrow[P, Q, DOT, ARROW, BIPRODUCT, EXPONENTIAL],
-    BIPRODUCT[P, Q] <: Biproduct[P, Q, DOT, ARROW, BIPRODUCT, EXPONENTIAL],
-    EXPONENTIAL[P, Q] <: Exponential[P, Q, DOT, ARROW, BIPRODUCT, EXPONENTIAL]
-  ] {
-    val source: DOT[X]
-    val target: DOT[Y]
-
-    def apply[W](arrow: ARROW[W, X]): ARROW[W, Y]
-  }
-
-  trait Biproduct[X, Y,
-    DOT[P] <: Dot[P, DOT, ARROW, BIPRODUCT, EXPONENTIAL],
-    ARROW[P, Q] <: Arrow[P, Q, DOT, ARROW, BIPRODUCT, EXPONENTIAL],
-    BIPRODUCT[P, Q] <: Biproduct[P, Q, DOT, ARROW, BIPRODUCT, EXPONENTIAL],
-    EXPONENTIAL[P, Q] <: Exponential[P, Q, DOT, ARROW, BIPRODUCT, EXPONENTIAL]
-  ] {
-    val leftProjection: ARROW[(X, Y), X]
-    val rightProjection: ARROW[(X, Y), Y]
-
-    def multiply[W](leftArrow: ARROW[W, X],
-                    rightArrow: ARROW[W, Y]): ARROW[W, (X, Y)]
-  }
-
-  trait Exponential[S, T,
-    DOT[P] <: Dot[P, DOT, ARROW, BIPRODUCT, EXPONENTIAL],
-    ARROW[P, Q] <: Arrow[P, Q, DOT, ARROW, BIPRODUCT, EXPONENTIAL],
-    BIPRODUCT[P, Q] <: Biproduct[P, Q, DOT, ARROW, BIPRODUCT, EXPONENTIAL],
-    EXPONENTIAL[P, Q] <: Exponential[P, Q, DOT, ARROW, BIPRODUCT, EXPONENTIAL]
-  ] {
-    val evaluation: BiArrow[S => T, S, T, DOT, ARROW, BIPRODUCT, EXPONENTIAL]
-
-    def transpose[W](multiArrow: BiArrow[W, S, T, DOT, ARROW, BIPRODUCT, EXPONENTIAL]): ARROW[W, S => T]
-  }
-
-  case class BiArrow[L, R, T,
-    DOT[P] <: Dot[P, DOT, ARROW, BIPRODUCT, EXPONENTIAL],
-    ARROW[P, Q] <: Arrow[P, Q, DOT, ARROW, BIPRODUCT, EXPONENTIAL],
-    BIPRODUCT[P, Q] <: Biproduct[P, Q, DOT, ARROW, BIPRODUCT, EXPONENTIAL],
-    EXPONENTIAL[P, Q] <: Exponential[P, Q, DOT, ARROW, BIPRODUCT, EXPONENTIAL]
-  ](
-     product: BIPRODUCT[L, R],
-     arrow: ARROW[(L, R), T])
 }
