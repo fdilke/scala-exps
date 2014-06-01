b785fb3acdeb6bf7c0694d94281f71c6a48253c0
Felix Dilke <fdilkeuk@yahoo.co.uk>
2014-06-01 14:23:26 +0100

    Anita's tabulation of stars showing where ** != *

--- src/main/scala/com/fdilke/anita/AnitaSandbox.scala
+++ src/main/scala/com/fdilke/anita/AnitaSandbox.scala
@@ -1,27 +1,54 @@
 package com.fdilke.anita
 
+import com.fdilke.util.FancyMultiplicationImplicits._
+
 object AnitaSandbox extends App {
-  val rows = 9
-  val columns = 6
+  def showTable[X](rowRange : Seq[Int], columnRange : Seq[Int], operator:String, f: (Int, Int) => X) {
 
+    val maxLength = (for (i <- rowRange ; j <- columnRange)
+      yield f(i, j).toString.length).max
 
-  print("  |")
-  for (i <- 1 to columns) {
-    print(" " + i + " ")
-  }
-  println("")
-  print("--+-")
+    println("Max Length = " + maxLength)
 
-  for (i <- 1 to columns) {
-    print("---")
-  }
-  println("")
-
-  for (i <- 1 to rows) {
-    print(" " + i + "|")
-    for (j <- 1 to columns) {
-      print("" + "%2d".format(i * j) + " ")
+      print(operator + " |")
+    for (i <- columnRange) {
+      print(" " + i + " ")
     }
     println("")
+    print("--+-")
+
+    for (i <- columnRange) {
+      print("---")
+    }
+    println("")
+
+    for (i <- rowRange) {
+      print(" " + i + "|")
+      for (j <- columnRange) {
+        print("" + "%2s".format(f(i, j)) + " ")
+      }
+      println("")
+    }
+    println
   }
+
+//  showTable(1 to 3, 1 to 3, "*", _ * _)
+//  showTable(2 to 7, -1 to 8, "+", _ + _)
+//  showTable(2 to 7, -1 to 8, "-", _ - _)
+
+  val testRange: Seq[Int] = 1 to 30
+
+
+
+  showTable(testRange, testRange, "*", _ * _)
+  //showTable(testRange, testRange, "F", _ ** _)
+
+  def different(a:Int, b:Int) =
+    if(a*b == a**b)
+      " "
+    else "*"
+
+//  showTable(testRange,testRange, "D", different)
+
 }
+
