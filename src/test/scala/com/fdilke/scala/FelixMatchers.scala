a65ac4150810a8267358d070e343a480fc8f139c
Casper devboxbackus.springer-sbm.com <devboxbackus.springer-sbm.com@springer.com>
2014-06-13 16:01:55 +0100

    experiments with ClassTag and matchers

--- src/test/scala/com/fdilke/scala/FelixMatchers.scala
+++ src/test/scala/com/fdilke/scala/FelixMatchers.scala
@@ -2,6 +2,8 @@ package com.fdilke.scala
 
 import org.scalatest.matchers.{MatchResult, BeMatcher, BePropertyMatcher, BePropertyMatchResult}
 
+import scala.reflect.ClassTag
+
 object FelixMatchers {
   def anInstanceOf[T](implicit manifest: Manifest[T]) = {
     val clazz = manifest.erasure.asInstanceOf[Class[T]]
@@ -13,12 +15,15 @@ object FelixMatchers {
     }
   }
 
-// TODO: investigate matcher mechanism with ClassTag like this:
-//def readValue[T:ClassTag](s:String):T = {
-//  val tClass = implicitly[ClassTag[T]].runtimeClass
-//  //implementation for different classes.
-//}
-
+  def ofType[T:ClassTag] = {
+    val tClass = implicitly[ClassTag[T]].runtimeClass
+    new BeMatcher[AnyRef] {
+      def apply(left: AnyRef) =
+        MatchResult(tClass.isAssignableFrom(left.getClass),
+          "an instance of " + tClass.getName,
+          "not an instance of " + tClass.getName)
+    }
+  }
 
 //  def distinct =
 //    new BeMatcher[AnyRef] {
--- src/test/scala/com/fdilke/scala/FelixMatchersTest.scala
+++ src/test/scala/com/fdilke/scala/FelixMatchersTest.scala
@@ -6,11 +6,15 @@ import FelixMatchers._
 
 class FelixMatchersTest extends FunSpec {
   describe("The anInstanceOf matcher") {
-    it("should match objects of specified type") {
+    it("should match simple objects of specified type") {
       "" shouldBe anInstanceOf[String]
-// and ideally:
-//      Array(2) shouldBe ( anInstanceOf[Array[Int]] )
-//      3 shouldBe ( anInstanceOf[Int] )
+    }
+
+    it("should match fancier objects of specified type") {
+      "" shouldBe ofType[String]
+      Array(2) shouldBe ofType[Array[Int]]
+// and ideally...
+//      3 shouldBe ofType[Int]
     }
   }
 }
