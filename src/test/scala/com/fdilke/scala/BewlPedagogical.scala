684c6e193bf95269c69c4543bbd966793f2f1ff6
Felix Dilke <fdilkeuk@yahoo.co.uk>
2015-03-30 21:32:10 +0100

    an apsirational category trait that Bewl's Topos might eventually inherit from

---
+++ src/test/scala/com/fdilke/scala/BewlPedagogical.scala
@@ -0,0 +1,20 @@
+package com.fdilke.scala
+
+object BewlPedagogical {
+
+  trait Category {
+    type DOT[S] <: Dot[S]
+    type ARROW[S, T] <: Arrow[S, T]
+
+    trait Dot[S] { dot: DOT[S] =>
+      val identity: ARROW[S, S]
+    }
+
+    trait Arrow[S, T] { arrow: ARROW[S, T] =>
+      val source: DOT[S]
+      val target: DOT[T]
+
+      def o[R](that: ARROW[R, S]) : ARROW[R, T]
+    }
+  }
+}
