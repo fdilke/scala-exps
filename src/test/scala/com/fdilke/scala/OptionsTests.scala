6f77fba02a756046de6adfeb0b13572afe52ab72
Felix Dilke <felix.dilke@springer-sbm.com>
2015-11-25 15:31:30 +0000

    Felix/Uthman: tests for options

---
+++ src/test/scala/com/fdilke/scala/OptionsTests.scala
@@ -0,0 +1,31 @@
+package com.fdilke.scala
+
+import org.scalatest.{Matchers, FunSpec}
+import Matchers._
+import org.scalatest.FunSpec
+
+class OptionsTests extends FunSpec  {
+  describe("Options") {
+    it("include None") {
+      None should not be 'defined
+    }
+    it("include Some") {
+      val two: Option[Int] = Some(2)
+      two should be ('defined)
+    }
+    it("can be fkatmapped") {
+      Some(Some(2)).flatten shouldBe Some(2)
+    }
+    it("can be mapped") {
+      Some(2) map { 1 + _ } shouldBe Some(3)
+    }
+    it("can be foreach'd") {
+      None.foreach{ identity }
+      var sum = 0
+      Some(2).foreach { i =>
+        sum = sum + i
+      }
+      sum shouldBe 2
+    }
+  }
+}
