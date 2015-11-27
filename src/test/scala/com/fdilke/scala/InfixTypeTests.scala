24c1abbcc7bb24472b32a592054569ab0532b76d
Felix Dilke <felix.dilke@springer-sbm.com>
2015-11-27 14:22:07 +0000

    Felix: experiments with infix types and making them right-associative

---
+++ src/test/scala/com/fdilke/scala/InfixTypeTests.scala
@@ -0,0 +1,44 @@
+package com.fdilke.scala
+
+import org.scalatest.FunSpec
+import org.scalatest.Matchers._
+
+class InfixTypeTests extends FunSpec {
+
+  type →[T, U] = T => U
+  type →:[T, U] = T → U
+  type ~:[A,B] = Map[A,B]
+  type ~~~:[A,B] = (A,B)
+
+  describe("Infix types") {
+    it("enable a funky arrow notation") {
+      def fun(n: Int) = n.toString
+      val arrow: Int → String = fun
+      def funWithArrows(arrow: Int → String) = arrow(3)
+      funWithArrows(arrow) shouldBe "3"
+    }
+    it("left-associate") {
+      val notACurriedArrow: Int → Boolean → String = {
+        i2b =>
+          if(i2b(2))
+            "Likes 2"
+          else
+            "Rejects 2"
+      }
+      notACurriedArrow(_ > 1) shouldBe "Likes 2"
+      notACurriedArrow(_ % 4 == 1) shouldBe "Rejects 2"
+    }
+    it("can also be made to right-associate") {
+      val curriedArrow: Int →: Boolean →: String =
+        i => b =>
+          if(b)
+            i.toString()
+          else
+            "Not " + i.toString()
+
+      curriedArrow(1)(true) shouldBe "1"
+      curriedArrow(3)(false) shouldBe "Not 3"
+    }
+  }
+}
+
