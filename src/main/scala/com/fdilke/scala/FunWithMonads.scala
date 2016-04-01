3f2caaba743ad8fba608c9d4428d6f1c5ec3ea25
felix <felix.dilke@springer.com>
2016-04-01 16:37:33 +0100

    initial version of single and double exponential monad code

---
+++ src/main/scala/com/fdilke/scala/FunWithMonads.scala
@@ -0,0 +1,41 @@
+package com.fdilke.scala
+
+import scala.languageFeature.higherKinds
+
+object FunWithMonads extends App {
+
+  def mu[X, H]: ((((X => H) => H) => H) => H) => ((X => H) => H) =
+    xhhhh => xh => xhhhh(_(xh))
+
+  abstract class Monad[M[X]] {
+    def eta[X](x: X): M[X]
+    def mu[X](mmx: M[M[X]]): M[X]
+  }
+
+  class ContextOf[H] {
+
+    type SingleExponential[X] = H => X
+
+    class SingleExponentialMonad extends Monad[SingleExponential] {
+      override def eta[X](x: X): SingleExponential[X] =
+        h => x
+
+      override def mu[X](
+        mmx: SingleExponential[SingleExponential[X]]
+      ) =
+        h => mmx(h)(h)
+    }
+
+    type DoubleExponential[X] = (X => H) => H
+
+    class DoubleExponentialMonad extends Monad[DoubleExponential] {
+      override def eta[X](x: X) =
+        _(x)
+
+      override def mu[X](
+        mmx: DoubleExponential[DoubleExponential[X]]
+      ) =
+        xh => mmx(_(xh))
+    }
+  }
+}
