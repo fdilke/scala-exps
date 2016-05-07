2bc2dcb079fa47efa5c52c94b49d27fe99367265
Felix Dilke <fdilkeuk@yahoo.co.uk>
2016-05-07 08:34:09 +0100

    experiments with extending inner traits

---
+++ src/test/scala/com/fdilke/scala/ExtendingInnerTraits.scala
@@ -0,0 +1,52 @@
+package com.fdilke.scala
+
+import org.scalatest.{Matchers, FreeSpec}
+import Matchers._
+
+class ExtendingInnerTraits extends FreeSpec {
+  
+  trait HasInner {
+    trait Inner {
+      def innerMethod = 2
+    }
+
+    def inner: Inner =
+      new Object with Inner
+  }
+  
+  "Inner traits" - {
+    "still work if you don't extend them" in {
+      class VanillaHasInner extends HasInner
+      val vanilla = new VanillaHasInner
+      vanilla.inner.innerMethod shouldBe 2
+    }
+
+    "can be extended" in {
+      class CustomHasInner extends HasInner {
+        trait Inner extends super.Inner {
+          def extraMethod = "x"
+        }
+        override def inner =
+          new Object with Inner
+      }
+      val custom = new CustomHasInner
+      custom.inner.innerMethod shouldBe 2
+      custom.inner.extraMethod shouldBe "x"
+    }
+
+    "can be extended and modified" in {
+      class CustomHasInner extends HasInner {
+        trait Inner extends super.Inner {
+          override def innerMethod = 3
+          def extraMethod = "x"
+        }
+        override def inner =
+          new Object with Inner
+      }
+      val custom = new CustomHasInner
+      custom.inner.innerMethod shouldBe 3
+      custom.inner.extraMethod shouldBe "x"
+    }
+  }
+
+}
