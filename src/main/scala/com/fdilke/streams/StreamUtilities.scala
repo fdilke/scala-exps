26a257716721160d4aeac6226b12a9fa27487d0a
felix <felix.dilke@springer.com>
2016-05-11 18:56:04 +0100

    upgrading stream utilities: stream all sequences from a given list

--- src/main/scala/com/fdilke/streams/StreamUtilities.scala
+++ src/main/scala/com/fdilke/streams/StreamUtilities.scala
@@ -2,6 +2,7 @@ package com.fdilke.streams
 
 import scala.Function.tupled
 import scala.collection.Iterator.iterate
+import scala.language.postfixOps
 
 object StreamUtilities {
 
@@ -23,4 +24,28 @@ object StreamUtilities {
     indexPairs map tupled {
       (m, n) => (left(m), right(n))
     }
+
+  def sequencesOfLength[X](
+    length: Int
+  ) (
+    base: X*
+  ): Seq[Seq[X]] =
+    if (length == 0)
+      Seq(Seq())
+    else
+      for {
+        tail <- sequencesOfLength(length - 1)(base :_*)
+        head <- base
+      } yield
+        head +: tail
+
+  def sequences[X](
+    base: X*
+  ): Stream[Seq[X]] =
+    if (base isEmpty)
+      Stream(Seq())
+    else
+      N flatMap { n =>
+        sequencesOfLength(n)(base :_*).toStream
+      }
 }
--- src/test/scala/com/fdilke/streams/StreamUtilitiesTest.scala
+++ src/test/scala/com/fdilke/streams/StreamUtilitiesTest.scala
@@ -31,4 +31,77 @@ class StreamUtilitiesTest extends FreeSpec {
       )
     }
    }
+
+  "Sequences" - {
+    "enumerates all sequences of a given length" - {
+      "for 0" in {
+        StreamUtilities.sequencesOfLength(0)() shouldBe Seq(
+          Seq()
+        )
+        StreamUtilities.sequencesOfLength(0)('a) shouldBe Seq(
+          Seq()
+        )
+      }
+      "for 1" in {
+        StreamUtilities.sequencesOfLength(1)() shouldBe Seq(
+        )
+        StreamUtilities.sequencesOfLength(1)('a) shouldBe Seq(
+          Seq('a)
+        )
+        StreamUtilities.sequencesOfLength(1)('a, 'b) shouldBe Seq(
+          Seq('a),
+          Seq('b)
+        )
+      }
+      "for 2" in {
+        StreamUtilities.sequencesOfLength(2)() shouldBe Seq(
+        )
+        StreamUtilities.sequencesOfLength(2)('a) shouldBe Seq(
+          Seq('a, 'a)
+        )
+        StreamUtilities.sequencesOfLength(2)('a, 'b) shouldBe Seq(
+          Seq('a, 'a),
+          Seq('b, 'a),
+          Seq('a, 'b),
+          Seq('b, 'b)
+        )
+      }
+      "for 3" in {
+        StreamUtilities.sequencesOfLength(3)() shouldBe Seq(
+        )
+        StreamUtilities.sequencesOfLength(3)('a) shouldBe Seq(
+          Seq('a, 'a, 'a)
+        )
+        StreamUtilities.sequencesOfLength(3)('a, 'b) shouldBe Seq(
+          Seq('a, 'a, 'a),
+          Seq('b, 'a, 'a),
+          Seq('a, 'b, 'a),
+          Seq('b, 'b, 'a),
+          Seq('a, 'a, 'b),
+          Seq('b, 'a, 'b),
+          Seq('a, 'b, 'b),
+          Seq('b, 'b, 'b)
+        )
+      }
+    }
+    "enumerates all sequences from a given base list" - {
+      "for empty" in {
+        StreamUtilities.sequences() shouldBe Seq(
+          Seq()
+        )
+      }
+      "for a singleton" in {
+        StreamUtilities.sequences(0).take(4) shouldBe Seq(
+          Seq(), Seq(0), Seq(0, 0), Seq(0, 0, 0)
+        )
+      }
+      "for a doubleton" in {
+        StreamUtilities.sequences(0, 1).take(7) shouldBe Seq(
+          Seq(),
+          Seq(0), Seq(1),
+          Seq(0, 0), Seq(1, 0), Seq(0, 1), Seq(1, 1)
+        )
+      }
+    }
+  }
 }
