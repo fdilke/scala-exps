1b952c310958e9beb81259c96272ee1e13a11d5c
Felix <knowledge-graph-dev@springernature.com>
2018-06-26 17:13:51 +0100

    Scala conundrum about case object matching raised by Fabio

---
+++ src/test/scala/com/fdilke/scala/FabioTest.scala
@@ -0,0 +1,29 @@
+package com.fdilke.scala
+
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers._
+
+sealed trait DbType[T]
+case object StringDbType extends DbType[String]
+case object IntDbType extends DbType[Int]
+
+trait FieldAdapter {
+  type U
+  def dbType: DbType[U]
+}
+
+class FabioTest extends FlatSpec {
+  "Pattern matching" should "work for a generalised ADT" in {
+    val x: FieldAdapter = new FieldAdapter {
+      override type U = String
+      override def dbType: DbType[String] = StringDbType
+    }
+
+    val dbTypeName = (x.dbType : DbType[_]) match {
+      case StringDbType => "varchar"
+      case IntDbType => "integer"
+    }
+
+    dbTypeName shouldBe "varchar"
+  }
+}
