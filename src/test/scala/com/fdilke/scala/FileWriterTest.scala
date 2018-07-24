24255cba9a41c8b54295626eb0097d6ac3cffb92
Felix Dilke <felix.dilke@springernature.com>
2018-07-24 14:29:12 +0100

    writing and reading files

---
+++ src/test/scala/com/fdilke/scala/FileWriterTest.scala
@@ -0,0 +1,28 @@
+package com.fdilke.scala
+
+import java.io.{BufferedReader, File, FileReader, FileWriter}
+
+import org.scalatest.{FunSpec, Matchers}
+import Matchers._
+import scala.language.implicitConversions
+
+class FileWriterTest extends FunSpec {
+  describe("A FileWriter") {
+    it("can write to a file") {
+      val newFile = new File("/tmp/my-test-file")
+      val writer = new FileWriter(newFile)
+      val myStrings = Seq(
+        "Hello Yasu",
+        "This is a message",
+        "Just for you"
+      )
+      myStrings foreach { text =>
+        writer.write(text + "\n")
+      }
+      writer.flush()
+
+      val reader = new BufferedReader(new FileReader(newFile))
+      reader.lines().toArray.toSeq shouldBe myStrings
+    }
+  }
+}
