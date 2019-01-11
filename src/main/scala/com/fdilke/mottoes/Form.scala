a495435de53771c80f2ae76b45c7a8d3864585a5
Felix Dilke <felix.dilke@springernature.com>
2019-01-11 10:57:38 +0000

    form initialization and stringifying

--- src/main/scala/com/fdilke/mottoes/Form.scala
+++ src/main/scala/com/fdilke/mottoes/Form.scala
@@ -4,7 +4,29 @@ sealed trait Form {
 
 }
 
-final class BasicForm(index: Int) extends Form {
-  override def toString : String =
-    ('A'.toInt + index).toChar.toString
+object Form {
+  val intForA: Int =
+    'A'.toInt
+
+  def apply(char: Char): Form =
+    BasicForm(char.toInt - intForA)
 }
+
+final case class BasicForm(
+  index: Int
+) extends Form {
+  override def toString : String =
+    (Form.intForA + index).toChar.toString
+}
+
+final case class CompoundForm(
+  src: Form,
+  tgt: Form
+) extends Form {
+  override def toString : String =
+    "(" + src.toString + " :> " + tgt.toString + ")"
+}
+
+//object StandardLetters {
+//  val A = BasicForm('A')
+//}
--- src/test/scala/com/fdilke/mottoes/FormTest.scala
+++ src/test/scala/com/fdilke/mottoes/FormTest.scala
@@ -4,10 +4,29 @@ import org.scalatest.FunSpec
 import org.scalatest.Matchers._
 
 class FormTest extends FunSpec {
-  describe("Forms") {
-    it("basic forms can be created and stringified") {
-      val basicForm : Form = new BasicForm(0)
+  describe("Basic forms") {
+    it("can be created and stringified") {
+      val basicForm: Form = BasicForm(0)
       basicForm.toString shouldBe "A"
     }
+
+    it("can be initialized from letters") {
+      BasicForm(0) shouldBe Form('A')
+    }
+//    it("have sane semantics of equality") {
+//
+//    }
+  }
+
+  describe("Compound forms") {
+
+    it("can be created and stringified") {
+      val basicFormB : Form = BasicForm(1)
+      val basicFormC : Form = BasicForm(2)
+      val compoundForm: Form = CompoundForm(basicFormB, basicFormC)
+      compoundForm.toString shouldBe "(B :> C)"
+    }
+
+    // add semantics of equality
   }
 }
