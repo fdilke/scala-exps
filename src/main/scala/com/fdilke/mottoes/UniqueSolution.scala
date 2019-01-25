bb46f0f62ab94e051231c9004eb4eef8db29d233
Felix Dilke <felix.dilke@springernature.com>
2019-01-25 12:22:09 +0000

    forms: added generic "unique solution finder" with mechanism to abandon search

---
+++ src/main/scala/com/fdilke/mottoes/UniqueSolution.scala
@@ -0,0 +1,30 @@
+package com.fdilke.mottoes
+
+object UniqueSolution {
+  implicit class FindUniqyeSolution[A](
+    candidates: Seq[A]
+  ) {
+    def hasUniqueSolution(criterion: PartialFunction[A, Boolean]): Boolean = {
+//      candidates.count { criterion } == 1
+      var numSolutions: Int = 0
+      candidates takeWhile { candidate =>
+        if (numSolutions <= 1) {
+          try {
+            if (criterion(candidate))
+              numSolutions += 1
+            true
+          } catch {
+            case _: AbandonUniqueSearchException =>
+              numSolutions = 2 // finish the search
+              false
+          }
+        } else
+          false
+      }
+      numSolutions == 1
+    }
+  }
+
+  class AbandonUniqueSearchException extends Exception
+}
+
---
+++ src/test/scala/com/fdilke/mottoes/UniqueSolutionTest.scala
@@ -0,0 +1,63 @@
+package com.fdilke.mottoes
+
+import com.fdilke.mottoes.StandardLetters._
+import org.scalatest.FunSpec
+import org.scalatest.Matchers._
+import FormMatchers._
+import UniqueSolution._
+
+import scala.collection.mutable
+import scala.collection.mutable.MutableList
+
+class UniqueSolutionTest extends FunSpec {
+
+  private val candidates: Seq[Int] =
+    Seq(1, 2, 3)
+
+  private def solveWith(criterion: Int => Boolean) =
+    candidates.hasUniqueSolution { case x => criterion(x) }
+
+  describe("The search for a unique solution") {
+    it("will detect a unique solution, if available") {
+      solveWith {
+        _ == 2
+      } shouldBe true
+    }
+
+    it("will detect when there are multiple solutions") {
+      solveWith {
+        _ > 1
+      } shouldBe false
+    }
+
+    it("will detect when there is no solution") {
+      solveWith {
+        _ < 0
+      } shouldBe false
+    }
+
+    it("will end as soon as we know the solution isn't unique") {
+      val seen: mutable.MutableList[Int] =
+        new mutable.MutableList[Int]
+
+      solveWith { candidate =>
+        seen += candidate
+        candidate > 0
+      } shouldBe false
+      seen shouldBe Seq(1, 2)
+    }
+
+    it("will be abandoned if the criterion signals it") {
+      val seen: mutable.MutableList[Int] =
+        new mutable.MutableList[Int]
+
+      solveWith { candidate =>
+        seen += candidate
+        if (candidate == 2)
+          throw new AbandonUniqueSearchException
+        candidate > 0
+      } shouldBe false
+      seen shouldBe Seq(1, 2)
+    }
+  }
+}
