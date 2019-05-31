6ba855402b8adb68fd50a72982ae0b578af0ce72
Felix Dilke <felix.dilke@springernature.com>
2019-05-31 14:40:31 +0100

    wordgraph, more indexed graph code

--- src/main/scala/com/fdilke/wordgraph/IndexedGraph.scala
+++ src/main/scala/com/fdilke/wordgraph/IndexedGraph.scala
@@ -3,10 +3,26 @@ package com.fdilke.wordgraph
 import scala.reflect.ClassTag
 
 case class IndexedGraph[NODE : ClassTag](
-  nodes: Set[NODE]
+  nodes: Set[NODE],
+  rawAdjacency: (NODE, NODE) => Boolean
 ) {
   private val nodeArray: Array[NODE] =
     nodes.toArray
+
+  val size: Int =
+    nodeArray.length
+
+  val adjacencyTable: Array[Array[Boolean]] =
+    (0 until size).toArray.map { indexLarger =>
+      val nodeLarger = value(indexLarger)
+      (0 until indexLarger).toArray.map { indexSmaller =>
+        val nodeSmaller = value(indexSmaller)
+        rawAdjacency(
+          nodeSmaller,
+          nodeLarger
+        )
+      }
+  }
 
   def indexOf(node: NODE): Int =
     nodeArray.indexOf(node)
@@ -14,6 +30,10 @@ case class IndexedGraph[NODE : ClassTag](
   def value(index: Int): NODE =
     nodeArray(index)
 
-  def size(): Int =
-    nodeArray.size
+  def isAdjacent(index1: Int, index2: Int): Boolean =
+    index1.compareTo(index2) match {
+      case 1 => adjacencyTable(index1)(index2)
+      case -1 => adjacencyTable(index2)(index1)
+      case _ => false
+    }
 }
--- src/test/scala/com/fdilke/wordgraph/IndexedGraphTest.scala
+++ src/test/scala/com/fdilke/wordgraph/IndexedGraphTest.scala
@@ -3,11 +3,17 @@ package com.fdilke.wordgraph
 import org.scalatest.{FunSpec, Matchers}
 import Matchers._
 
+import scala.collection.mutable
+
 class IndexedGraphTest extends FunSpec {
-  private val graph = IndexedGraph(Set("foo", "bar", "baz"))
 
   describe("Indexed graphs") {
     it("create an index of their vertex sets") {
+      val graph =
+        IndexedGraph(
+          Set("foo", "bar", "baz"),
+          alwaysAdjacent
+        )
       val foo = graph.indexOf("foo")
       val bar = graph.indexOf("bar")
       val baz = graph.indexOf("baz")
@@ -18,7 +24,51 @@ class IndexedGraphTest extends FunSpec {
     }
 
     it("measure the size of the graph") {
-      graph.size() shouldBe 3
+      IndexedGraph(
+        Set("foo", "bar", "baz"),
+        alwaysAdjacent
+      ).size shouldBe 3
+    }
+
+    it("eagerly caches adjacency information") {
+      val adjacencyHelper = new AdjacencyHelper
+      adjacencyHelper.flagAdjacent("foo", "bar")
+      adjacencyHelper.flagAdjacent("bar", "baz")
+      val graph =
+        IndexedGraph(
+          Set("foo", "bar", "baz", "corge"),
+          adjacencyHelper.isAdjacent
+        )
+      System.err.println(s"queriedAdj = ${adjacencyHelper.queriedAdjacencies}")
+      adjacencyHelper.queriedAdjacencies.size shouldBe 6
+      val foo = graph.indexOf("foo")
+      val bar = graph.indexOf("bar")
+      val baz = graph.indexOf("baz")
+      graph.isAdjacent(foo, bar) shouldBe true
+      graph.isAdjacent(foo, baz) shouldBe false
+      adjacencyHelper.queriedAdjacencies.size shouldBe 6
+    }
+  }
+
+  private def alwaysAdjacent(str1: String, str2: String) =
+    true
+
+  class AdjacencyHelper {
+    val adjacencies: mutable.Set[(String, String)] =
+      mutable.Set[(String, String)]()
+
+    val queriedAdjacencies: mutable.Buffer[(String, String)] =
+      mutable.Buffer[(String, String)]()
+
+    def flagAdjacent(str1: String, str2: String): Unit = {
+      val edge = str1 -> str2
+      adjacencies.add(edge)
+    }
+
+    def isAdjacent(str1: String, str2: String): Boolean = {
+      val edge = str1 -> str2
+      queriedAdjacencies.append(edge)
+      adjacencies.contains(edge)
     }
   }
 }
