9b1bfdf057a214f5935822d5dedf30e3e033b8cd
Felix Dilke <felix.dilke@springernature.com>
2019-06-14 10:44:52 +0100

    further tidyings of the permutation group stuff

--- src/test/scala/com/fdilke/permutations/GenerateGroupTest.scala
+++ src/test/scala/com/fdilke/permutations/GenerateGroupTest.scala
@@ -6,7 +6,7 @@ import Matchers._
 class GenerateGroupTest extends FunSpec {
   describe("Generating groups") {
     it("should work even with the empty set as a basis") {
-      GenerateGroup.apply().set should be (
+      GenerateGroup().set should be (
         Set(Permutation.identity)
       )
     }
@@ -14,14 +14,14 @@ class GenerateGroupTest extends FunSpec {
     it("should give cyclic groups when started with a single element") {
       val id = Permutation(0,1,2,3)
       val a = Permutation(1,0,2,3)
-      GenerateGroup.apply(a).set should be (Set(id, a))
+      GenerateGroup(a).set should be (Set(id, a))
     }
 
     it("should give the Klein four-group when started with appropriate generators") {
       val id = Permutation(0,1,2,3)
       val a = Permutation(1,0,2,3)
       val b = Permutation(0,1,3,2)
-      GenerateGroup.apply(a, b).set should be (
+      GenerateGroup(a, b).set should be (
         Set(id, a, b, a(b))
       )
     }
@@ -29,7 +29,7 @@ class GenerateGroupTest extends FunSpec {
     it("should give the dihedral group of order 18 on two generators") {
       val a = Permutation(1,2,3,4,5,6,7,8,0)
       val b = Permutation(0,8,7,6,5,4,3,2,1)
-      GenerateGroup.apply(a, b).set.size should be (18)
+      GenerateGroup(a, b).set.size should be (18)
     }
   }
 }
--- src/test/scala/com/fdilke/permutations/PermutationTest.scala
+++ src/test/scala/com/fdilke/permutations/PermutationTest.scala
@@ -10,18 +10,18 @@ class PermutationTest extends FunSpec {
       val id = Permutation.identity
       val r = Permutation(1,2,0)
 
-      r(id) should be (r)
-      id(r) should be (r)
+      r(id) shouldBe r
+      id(r) shouldBe r
     }
 
     it("should act on numbers even if they're out of range") {
-      Permutation.identity(0) should be(0)
-      Permutation.identity(5) should be(5)
+      Permutation.identity(0) shouldBe 0
+      Permutation.identity(5) shouldBe 5
 
       val s = Permutation(1,0)
-      s(0) should be(1)
-      s(1) should be(0)
-      s(2) should be(2)
+      s(0) shouldBe 1
+      s(1) shouldBe 0
+      s(2) shouldBe 2
     }
 
     it("should compose as though acting on the right") {
@@ -30,45 +30,45 @@ class PermutationTest extends FunSpec {
       val ab = Permutation(1,3,0,2)
       val ba = Permutation(2,0,3,1)
 
-      a(b) should be (ab)
-      b(a) should be (ba)
+      a(b) shouldBe ab
+      b(a) shouldBe ba
       ab should not be ba
     }
 
     it("should have equality semantics depending only on elements shifted") {
 
-      Permutation(0,1,2) should be(Permutation.identity)
+      Permutation(0,1,2) shouldBe Permutation.identity
 
       val a = Permutation(1,0)
       val b = Permutation(1,0,2)
       val c = Permutation(2,1,0)
 
-      a should be(a)
-      a should be(b)
-      a(a) should be (Permutation.identity)
+      a shouldBe a
+      a shouldBe b
+      a(a) shouldBe Permutation.identity
       a should not be c
 
-      a.degree should be(2)
+      a.degree shouldBe 2
     }
 
     it("should get permutations into canonical form") {
-      Permutation.essentialDegree(0,1) should be(0)
-      Permutation.essentialDegree(1,0) should be(2)
-      Permutation.essentialDegree(1,0,2) should be(2)
-      Permutation.essentialDegree(1,0,2) should be(2)
-      Permutation.essentialDegree(1,3,0,2) should be(4)
+      Permutation.essentialDegree(0,1) shouldBe 0
+      Permutation.essentialDegree(1,0) shouldBe 2
+      Permutation.essentialDegree(1,0,2) shouldBe 2
+      Permutation.essentialDegree(1,0,2) shouldBe 2
+      Permutation.essentialDegree(1,3,0,2) shouldBe 4
 
-      Permutation.canonical(0,1,2) should be(Seq[Int]())
-      Permutation.canonical(1,0,2) should be(Seq(1,0))
-      Permutation.canonical(1,3,0,2) should be(Seq(1,3,0,2))
+      Permutation.canonical(0,1,2) shouldBe Seq[Int]()
+      Permutation.canonical(1,0,2) shouldBe Seq(1,0)
+      Permutation.canonical(1,3,0,2) shouldBe Seq(1,3,0,2)
     }
 
     it("should test parity") {
-      Permutation.identity.parity should be (1)
-      Permutation(1,0).parity should be (-1)
-      Permutation(1,2,0).parity should be (1)
-      Permutation(2,0,1).parity should be (1)
-      Permutation(2,0,3,1).parity should be (-1)
+      Permutation.identity.parity shouldBe 1
+      Permutation(1,0).parity shouldBe -1
+      Permutation(1,2,0).parity shouldBe 1
+      Permutation(2,0,1).parity shouldBe 1
+      Permutation(2,0,3,1).parity shouldBe -1
     }
   }
 }
