f613a5738b3f01bd0655e2f5e9bdab92a19db437
Felix Dilke <felix.dilke@springernature.com>
2019-07-12 18:30:51 +0100

    calculating sets of right coset representatives

---
+++ src/main/scala/com/fdilke/varsymm/RightCosetRepresentatives.scala
@@ -0,0 +1,26 @@
+package com.fdilke.varsymm
+
+object RightCosetRepresentatives {
+  def apply[G](
+    group: Group[G]
+  )(
+    smallSubgroup: group.Subgroup,
+    bigSubgroup: group.Subgroup
+  ): Seq[G] = {
+    val index: Int =
+      bigSubgroup.order / smallSubgroup.order
+
+    Iterator.iterate(
+      Seq(group.unit) -> smallSubgroup.elements
+    ) (Function.tupled { (reps, repsH) =>
+      val newRep: G =
+        bigSubgroup.elements.diff(
+          repsH
+        ).head
+      val newRepH = smallSubgroup.elements.map { h =>
+        group.multiply(newRep, h)
+      }
+      (reps :+ newRep) -> (repsH union newRepH)
+    }).drop(index - 1).toSeq.head._1
+  }
+}
---
+++ src/test/scala/com/fdilke/varsymm/RightCosetRepresentativesTest.scala
@@ -0,0 +1,72 @@
+package com.fdilke.varsymm
+
+import org.scalatest.FunSpec
+import org.scalatest.Matchers._
+
+class RightCosetRepresentativesTest extends FunSpec {
+  private val group = DihedralGroup(6)
+
+  describe("A set of right coset representatives") {
+    it("is just 1 in the trivial case H=G") {
+      RightCosetRepresentatives(group)(
+        group.wholeGroup,
+        group.wholeGroup
+      ) shouldBe Seq(
+        group.unit
+      )
+    }
+
+    it("is just the group elements in the trivial case H=1") {
+      val representatives =
+        RightCosetRepresentatives(group)(
+          group.trivialSubgroup,
+          group.wholeGroup
+        )
+      representatives should not be(empty)
+      representatives.head shouldBe group.unit
+      representatives.toSet shouldBe group.elements
+    }
+
+    it("is sensible for the case G:H = 2") {
+      val subgroup =
+        group.generateSubgroup(
+          DihedralSymmetry(reflect = false, 1)
+        )
+      val representatives =
+        RightCosetRepresentatives(group)(
+          subgroup,
+          group.wholeGroup
+        )
+      representatives should have size 2
+      representatives.head shouldBe group.unit
+      subgroup.elements should not contain(representatives(1))
+    }
+
+    it("is sensible for the case G:H = 3") {
+      val subgroup =
+        group.generateSubgroup(
+          group.elements.find { x =>
+            group.orderOf(x) == 2
+          }.get
+        )
+      val representatives =
+        RightCosetRepresentatives(group)(
+          subgroup,
+          group.wholeGroup
+        )
+      representatives should have size 3
+      representatives.head shouldBe group.unit
+      val x = representatives(1)
+      val y = representatives(2)
+      def timesH(z: DihedralSymmetry): Set[DihedralSymmetry] =
+        subgroup.elements.map { h =>
+          group.multiply(z, h)
+        }
+      val xH = timesH(x)
+      val yH = timesH(y)
+      xH intersect yH shouldBe empty
+      xH intersect subgroup.elements shouldBe empty
+      yH intersect subgroup.elements shouldBe empty
+    }
+  }
+}
