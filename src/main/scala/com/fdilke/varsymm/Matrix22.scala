73062b571e79285e5a99f2f93f1ede451663fcf5
Felix Dilke <felix.dilke@springernature.com>
2019-07-26 15:00:33 +0100

    multiply matrices and homomorphically map angles onto them

--- src/main/scala/com/fdilke/varsymm/Matrix22.scala
+++ src/main/scala/com/fdilke/varsymm/Matrix22.scala
@@ -6,10 +6,19 @@ case class Matrix22(
   a21: Double,
   a22: Double
 ) {
-
+  def *(that: Matrix22): Matrix22 =
+    Matrix22(
+      a11*that.a11 + a12*that.a21,
+      a11*that.a12 + a12*that.a22,
+      a21*that.a11 + a22*that.a21,
+      a21*that.a12 + a22*that.a22
+    )
 }
 
 object Matrix22 {
+  val identity: Matrix22 =
+    Matrix22(1, 0, 0, 1)
+
   def withinTolerance(
      matrix: Matrix22,
      matrix2: Matrix22,
@@ -26,4 +35,10 @@ object Matrix22 {
     tolerance: Double
  ) : Boolean =
     Math.abs(value - value2) < tolerance
+
+  def rotation(theta: Double): Matrix22 = {
+    val c = Math.cos(theta)
+    val s = Math.sin(theta)
+    Matrix22(c, -s, s, c)
+  }
 }
--- src/test/java/com/fdilke/varsymm/Matrix22Test.scala
+++ src/test/java/com/fdilke/varsymm/Matrix22Test.scala
@@ -4,6 +4,8 @@ import org.scalatest.FunSpec
 import org.scalatest.Matchers._
 
 class Matrix22Test extends FunSpec {
+  private val TOLERANCE = 0.001
+
   describe("2x2 matrices") {
     it("can be compared w.r.t. a tolerance") {
       val matrix = Matrix22(1.2, 2.3, 3.4, 4.5)
@@ -11,6 +13,64 @@ class Matrix22Test extends FunSpec {
 
       Matrix22.withinTolerance(matrix, matrix2, 0.002) shouldBe false
       Matrix22.withinTolerance(matrix, matrix2, 0.01) shouldBe true
+    }
+
+    it("can be multiplied") {
+      val a = Matrix22(
+        2,  3,
+        4,  5
+      )
+      val b = Matrix22(
+        6,  7,
+        8,  9
+      )
+      Matrix22.withinTolerance(
+        a * b,
+        Matrix22(
+          36, 41,
+          64, 73
+        ),
+        TOLERANCE
+      ) shouldBe true
+    }
+
+    it("can embody rotations") {
+      val c = 0.5
+      val s = Math.sqrt(3)/2
+      Matrix22.withinTolerance(
+        Matrix22.rotation(
+          Math.PI / 3
+        ),
+        Matrix22(
+          c, -s,
+          s, c
+        ),
+        TOLERANCE
+      ) shouldBe true
+    }
+
+    it("can embody rotations homomorphically") {
+      Matrix22.withinTolerance(
+        Matrix22.rotation(
+          0
+        ),
+        Matrix22.identity,
+        TOLERANCE
+      ) shouldBe true
+
+      val angle1 = Math.PI / 4
+      val angle2 = Math.PI / 5
+      Matrix22.withinTolerance(
+        Matrix22.rotation(
+          angle1
+        ) * Matrix22.rotation(
+          angle2
+        ),
+        Matrix22.rotation(
+          angle1 + angle2
+        ),
+        TOLERANCE
+      ) shouldBe true
     }
   }
 
