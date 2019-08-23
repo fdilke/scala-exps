d122a98f18279cbc70d5c2d780b7e6e05198ff9b
Felix Dilke <felix.dilke@springernature.com>
2019-08-23 10:52:57 +0100

    initial "dihedral app" Swing code

--- src/main/scala/com/fdilke/varsymm/DihedralApp.scala
+++ src/main/scala/com/fdilke/varsymm/DihedralApp.scala
@@ -1,6 +1,11 @@
 package com.fdilke.varsymm
 
+import java.awt.{Color, Graphics, GraphicsDevice, GraphicsEnvironment}
+
 import GroupSugar._
+import javax.swing.WindowConstants
+import javax.swing.JFrame
+import javax.swing.JPanel
 
 object DihedralApp extends App {
   implicit val group = DihedralGroup(12)
@@ -19,4 +24,25 @@ object DihedralApp extends App {
       (if (hasCubeRoot) "³" else "")
     )
   }
+}
+
+object AltDihedralApp extends App {
+  new JFrame { frame =>
+    setContentPane(new JPanel {
+      override def paintComponent(g: Graphics): Unit = {
+        g.setColor(Color.PINK)
+        g.fillRect(20, 20, 100, 200) // Draw on g here e.g.
+      }
+    })
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+    setUndecorated(true)
+
+    pack()
+
+    val device: GraphicsDevice =
+      GraphicsEnvironment.getLocalGraphicsEnvironment.getScreenDevices()(0)
+
+    device.setFullScreenWindow(frame)
+    setVisible(true)
+ }
 }
