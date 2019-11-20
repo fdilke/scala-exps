c091323c483a39dada9f9a4976988ad0824302d2
Felix Dilke <felix.dilke@springernature.com>
2019-11-20 17:18:43 +0000

    continued fractions for powers of gamma

--- src/main/scala/com/fdilke/approx/ContinuingObsession.scala
+++ src/main/scala/com/fdilke/approx/ContinuingObsession.scala
@@ -5,4 +5,12 @@ object ContinuingObsession extends App {
 
   val mu = Math.log(2) / Math.log(1.5)
   println(ContinuedFraction(mu, 8))
+
+  val gamma = Math.pow(2, 1/12.0)
+  for { n <- 1 until 12 } {
+    val gn = Math.pow(gamma, n)
+    val frac = ContinuedFraction(gn, 6)
+    val simplest: Rational = frac.approximants.find { _.denominator > 3 }.get
+    println(s"g^$n\t$simplest")
+  }
 }
