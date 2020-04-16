6c38b24955c77e68b368ddcd57e842dd1742708b
Felix Dilke <fdilkeuk@yahoo.co.uk>
2020-04-16 17:26:57 +0100

    minor experiments with implicits

--- src/test/scala/com/fdilke/scala/ImplicitFrenzyTest.scala
+++ src/test/scala/com/fdilke/scala/ImplicitFrenzyTest.scala
@@ -4,18 +4,29 @@ import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers._
 
 class ImplicitFrenzyTest extends AnyFunSpec {
-  describe("Implicit conversions with implicit parameters") {
-    it("work just fine") {
+  describe("Implicits support") {
+    it("conversions with implicit parameters") {
       case class Prefix(prefix: String)
       object Scope {
+
         implicit class Foo(text: String) {
           def extensionMethod()(implicit prefix: Prefix) =
             (prefix.prefix + text).length
         }
+
       }
       import Scope._
       implicit val prefix: Prefix = Prefix("//")
       "xyz".extensionMethod() shouldBe 5
+    }
+    it("automatic use of builder functions if available") {
+      trait Wellness[T]
+      object Wellness {
+        implicit def standardWellness[T]: Wellness[T] =
+          new Wellness[T] {}
+      }
+      val wellness: Wellness[Seq[Int]] =
+        implicitly[Wellness[Seq[Int]]]
     }
   }
 }
